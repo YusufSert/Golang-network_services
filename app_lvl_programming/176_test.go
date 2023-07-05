2ecaa6ea07993c08040535d82fad436b0da0c356
YusufSert <yusufcansert123@gmail.com>
2023-07-05 22:33:12 +0300

    app_lvl_programming - HTTP client

---
+++ app_lvl_programming/176_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+
+
+
+
+func blockIndefinitely(w http.ResponseWriter, r*http.Request) {
+	select{}
+}
+
+func TestBlockIndefinitely(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(blockIndefinitely))
+	_, _ = http.Get(ts.URL)
+	t.Fatal("Client did not indefinitely block")
+}
+
---
+++ app_lvl_programming/179_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type User struct {
+	First	string
+	Last	string
+}
+
+func handlePostUser(t*testing.T) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func(r io.ReadCloser) {
+			_, _ = io.Copy(ioutil.Discard, r)
+			_ = r.Close()
+		}(r.Body)
+
+		if r.Method != http.MethodPost {
+			http.Error(w, "", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var u User
+		err := json.NewDecoder(r.Body).Decode(&u)
+		if err != nil {
+			t.Error(err)
+			http.Error(w, "Decode Failed", http.StatusBadRequest)
+			return
+		}
+		
+		w.WriteHeader(http.StatusAccepted)
+		
+		fmt.Printf("%+v\n", u)
+	}
+}
+
+
+func TestPostUser(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(handlePostUser(t)))
+	defer ts.Close()
+
+	resp, err := http.Get(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Fatalf("expected status %d; actual status %d",
+			http.StatusMethodNotAllowed, resp.StatusCode)
+	}
+
+	buf := new(bytes.Buffer)
+	u := User{First: "Adam", Last: "WoodBeck"}
+	err = json.NewEncoder(buf).Encode(&u)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err = http.Post(ts.URL, "application/json", buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusAccepted {
+		t.Fatalf("expected status %d; actual status %d",http.StatusAccepted, resp.StatusCode)
+	}
+	_ = resp.Body.Close()
+}
